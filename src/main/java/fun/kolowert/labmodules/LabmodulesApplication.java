dba005d23caf915859e74ffdf4266bdb9be16491
taras kohut <tiarkey@yahoo.com>
2022-01-24 16:49:03 +0200

    init

---
+++ src/main/java/fun/kolowert/labmodules/LabmodulesApplication.java
@@ -0,0 +1,13 @@
+package fun.kolowert.labmodules;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabmodulesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabmodulesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fun/kolowert/labmodules/LabmodulesApplicationTests.java
@@ -0,0 +1,13 @@
+package fun.kolowert.labmodules;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabmodulesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
